4e64f6e37983433e9db8ed08b3b0c5b7c99676b8
Yudev <yud7natera@gmail.com>
2021-02-18 11:56:48 -0300

    Comentando líneas de código de ejemplo

--- src/js/home.js
+++ src/js/home.js
@@ -1,27 +1,27 @@
-console.log('hola mundo!');
-const noCambia = "Leonidas";
+// console.log('hola mundo!');
+// const noCambia = "Leonidas";
 
-let cambia = "@LeonidasEsteban"
+// let cambia = "@LeonidasEsteban"
 
-function cambiarNombre(nuevoNombre) {
-  cambia = nuevoNombre
-}
+// function cambiarNombre(nuevoNombre) {
+//   cambia = nuevoNombre
+// }
 
-const getUserAll = new Promise(function(todoBien, todoMal) {
-  // llamar a un api
-  setTimeout(function() {
-    // luego de 3 segundos
-    todoBien('se acabó el tiempo');
-  }, 5000)
-})
+// const getUserAll = new Promise(function(todoBien, todoMal) {
+//   // llamar a un api
+//   setTimeout(function() {
+//     // luego de 3 segundos
+//     todoBien('se acabó el tiempo');
+//   }, 5000)
+// })
 
-const getUser = new Promise(function(todoBien, todoMal) {
-  // llamar a un api
-  setTimeout(function() {
-    // luego de 3 segundos
-    todoBien('se acabó el tiempo 3');
-  }, 3000)
-})
+// const getUser = new Promise(function(todoBien, todoMal) {
+//   // llamar a un api
+//   setTimeout(function() {
+//     // luego de 3 segundos
+//     todoBien('se acabó el tiempo 3');
+//   }, 3000)
+// })
 
 // getUser
 //   .then(function() {
@@ -31,40 +31,40 @@ const getUser = new Promise(function(todoBien, todoMal) {
 //     console.log(message)
 //   })
 
-Promise.race([
-  getUser,
-  getUserAll,
-])
-.then(function(message) {
-  console.log(message);
-})
-.catch(function(message) {
-  console.log(message)
-})
+// Promise.race([
+//   getUser,
+//   getUserAll,
+// ])
+// .then(function(message) {
+//   console.log(message);
+// })
+// .catch(function(message) {
+//   console.log(message)
+// })
 
 
 
-$.ajax('https://randomuser.me/api/sdfdsfdsfs', {
-  method: 'GET',
-  success: function(data) {
-    console.log(data)
-  },
-  error: function(error) {
-    console.log(error)
-  }
-})
+// $.ajax('https://randomuser.me/api/sdfdsfdsfs', {
+//   method: 'GET',
+//   success: function(data) {
+//     console.log(data)
+//   },
+//   error: function(error) {
+//     console.log(error)
+//   }
+// })
 
-fetch('https://randomuser.me/api/dsfdsfsd')
-  .then(function (response) {
-    // console.log(response)
-    return response.json()
-  })
-  .then(function (user) {
-    console.log('user', user.results[0].name.first)
-  })
-  .catch(function() {
-    console.log('algo falló')
-  });
+// fetch('https://randomuser.me/api/dsfdsfsd')
+//   .then(function (response) {
+//     // console.log(response)
+//     return response.json()
+//   })
+//   .then(function (user) {
+//     console.log('user', user.results[0].name.first)
+//   })
+//   .catch(function() {
+//     console.log('algo falló')
+//   });
 
 
 (async function load() {
